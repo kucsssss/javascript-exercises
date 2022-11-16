0c5aad17e2cd03181706036755e3815bd639185f
kucsssss <117690564+kucsssss@users.noreply.github.com>
2022-11-16 15:51:45 +0100

    Done the exercises

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const revArray = [];
+    const length = str.length - 1;
 
+    if(!str || str.length < 2 || typeof str!== 'string') {
+        return '';
+    }
+
+    for (let i = length; i >= 0; i--) {
+        revArray.push(str[i]);     
+    }
+    return revArray.join('');
 };
 
 // Do not edit below this line
